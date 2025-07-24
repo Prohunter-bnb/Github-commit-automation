12cd7034ea2aa1bcd9bc2665121b348263055892
Utkarsh Singh <jee60101@gmail.com>
2025-07-24 10:59:26 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,12 +1,12 @@
 <?php
 // GitHub personal access token (keep this secret!)
-define("GITHUB_TOKEN", "Github Access Token");
+define("GITHUB_TOKEN", "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");
 
 // Your GitHub username
-define("GITHUB_USERNAME", "Usr name");
+define("GITHUB_USERNAME", "sujan725");
 
 // Owner of the repo (usually same as username)
-define("GITHUB_OWNER", "Usr name");
+define("GITHUB_OWNER", "sujan725");
 
 // GitHub API URL
 define("GITHUB_API_URL", "https://api.github.com");
